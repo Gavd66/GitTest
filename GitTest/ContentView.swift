97bd975e4f1b90cb533758ef0930e0dfdfc3e8f9
gav <gavd66@me.com>
2022-02-18 15:46:26 +1030

    Roll back color change

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -14,7 +14,6 @@ struct ContentView: View {
             Button("Show modal") {
                 self.isPresented = true
             }
-            .foregroundColor(.red)
             .sheet(isPresented: $isPresented) {
                 ModalView()
             }
