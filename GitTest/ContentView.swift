0fada377478f619d7a7abf716a064bad0444e536
gav <gavd66@me.com>
2022-02-18 16:09:14 +1030

    Change text to blue

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             NavigationLink(destination: ModalView()) {
                 Text("Go Next")
             }
+            .foregroundColor(.blue)
             .navigationTitle("Xcode and Git")
         }
     }
